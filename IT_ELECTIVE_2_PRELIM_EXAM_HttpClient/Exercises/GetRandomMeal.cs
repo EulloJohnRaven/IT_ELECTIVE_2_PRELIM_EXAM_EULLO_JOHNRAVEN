2acd673a1ab0767fba9f1a9e1f721b4820a5ec38
EulloJohnRaven <cancergaming123456@gmail.com>
2026-07-11 08:53:27 +0800

    GetRandomMeal Updated

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetRandomMeal.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetRandomMeal.cs
@@ -5,7 +5,7 @@ namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 //
 // Your task:
 // 1. Use the HttpClient to send a GET request to the URL above
- // 2. Read the response as a string
+// 2. Read the response as a string
 // 3. Assert that the status code is 200 OK
 // 4. Assert that the response body is not null or empty
 //
@@ -18,10 +18,16 @@ public static class GetRandomMeal
     {
         // TODO: Send GET request to https://themealdb.com/api/json/v1/1/random.php
         var response = await client.GetAsync("https://themealdb.com/api/json/v1/1/random.php");
-        // TODO: Read the response content as a string
-        // TODO: Assert status code is 200 OK
-        // TODO: Assert response body is not null or empty
 
-        throw new NotImplementedException();
+        // TODO: Assert status code is 200 OK
+        if (!response.IsSuccessStatusCode)
+            throw new Exception($"Status code is {response.StatusCode}, expected 200 OK");
+
+        // TODO: Read the response content as a string
+        var body = await response.Content.ReadAsStringAsync();
+
+        // TODO: Assert response body is not null or empty
+        if (string.IsNullOrWhiteSpace(body))
+            throw new Exception("Response body is null or empty");
     }
 }
