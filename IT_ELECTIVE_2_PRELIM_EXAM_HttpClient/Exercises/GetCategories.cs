8ea91b806cd8a690d74b1e68eb59d14d60cd8176
EulloJohnRaven <cancergaming123456@gmail.com>
2026-07-11 09:02:27 +0800

    Await GetCategory

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetCategories.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetCategories.cs
@@ -15,6 +15,8 @@ public static class GetCategories
     public static async Task Run(System.Net.Http.HttpClient client)
     {
         // TODO: Send GET request to https://themealdb.com/api/json/v1/1/categories.php
+        var response = await client.GetAsync("https://themealdb.com/api/json/v1/1/lookup.php?i=52771");
+
         // TODO: Assert status code is 200 OK
         // TODO: Parse the response JSON
         // TODO: Assert the "categories" array has more than 0 items
